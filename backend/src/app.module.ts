494d4225eb9dca2ac0fcf1b7112d5344d421fb32
raj <67539971+rajzzz@users.noreply.github.com>
2026-05-31 17:53:45 +0530

    Backend Setup, DDD Structure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { WorkspaceModule } from './workspace/workspace.module';
+import { OrganizationModule } from './organization/organization.module';
+import { ProjectModule } from './project/project.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, WorkspaceModule, OrganizationModule, ProjectModule, TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
